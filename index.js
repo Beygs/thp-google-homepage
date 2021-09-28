d68b685e191249ce53ec1ac8df40c2828dd4c435
Beygs <boris.gilles@hotmail.fr>
2021-09-28 19:12:43 +0200

    Added some cool effects and easter eggs

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const hamburger = document.querySelector(".hamburger");
 const hamburgerOpened = document.querySelector(".hamburger-opened");
+const closeHamburger = document.querySelector(".close-hamburger");
 
 hamburger.addEventListener("click", () => {
-  console.log(hamburger);
   if (hamburgerOpened.getAttribute("style") === "display: none") {
+    closeHamburger.setAttribute("style", "display: block");
     hamburgerOpened.setAttribute("style", "display: inline-block");
   } else {
+    closeHamburger.setAttribute("style", "display: none");
     hamburgerOpened.setAttribute("style", "display: none");
   }
 });
+
+closeHamburger.addEventListener("click", () => {
+  closeHamburger.setAttribute("style", "display: none");
+  hamburgerOpened.setAttribute("style", "display: none");
+})
